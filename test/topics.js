6a4ef1aa46d88712515a5bcdd067db529fdb9f25
Joy Song <joysong@joys-air-2.wifi.local.cmu.edu>
2024-01-24 17:54:08 -0500

    Revert "added coverage for line 14 of move.js"
    This reverts commit e7a3eb4f9b44f24c10e1b0bcfb51b86f4b9cd4f0.

--- test/topics.js
+++ test/topics.js
@@ -57,14 +57,6 @@ describe('Topic\'s', () => {
         };
     });
 
-    it('should throw an error if data is invalid', async () => {
-        try {
-            await socketTopics.move({}, null);
-        } catch (err) {
-            assert.strictEqual(err.message, '[[error:invalid-data]]');
-        }
-    });
-
     describe('.post', () => {
         it('should fail to create topic with invalid data', async () => {
             try {
